01f77866b8d8d034a842407df47a31a1fba04a76
alihou-junior <alihou2002@gmail.com>
2025-01-11 20:31:50 +0100

    rajout noeud

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/mindmap', (req, res) => {
 app.get('/mindmap/specific_audience',(req, res)=> {
     const spec_aud = {
         name: "Specific Audience",
-        url : "#",
+        url : "/mindmap/specific_audience",
         children: [
             { name: "Kids", url:"/mindmap/specific_audience/kids"},
             { name: "older Adults", url:"/mindmap/specific_audience/older_adults" },
@@ -53,7 +53,7 @@ app.get('/mindmap/specific_audience',(req, res)=> {
 app.get('/mindmap/conferences_and_interdisciplinary',(req, res)=> {
     const conf_inter = {
         name: "Conferences and Interdisciplinary Portfolio",
-        url : "#",
+        url : "/mindmap/conferences_and_interdisciplinary",
         children: [
             { name: "interdisciplinary contributions", url:"/mindmap/conferences_and_interdisciplinary/interdisciplinary" },
             { name: "talks and presentations", url:"/mindmap/conferences_and_interdisciplinary/talks_presentations"  },]
@@ -64,7 +64,7 @@ app.get('/mindmap/conferences_and_interdisciplinary',(req, res)=> {
 app.get('/mindmap/Educational_robotic_computational_thinking',(req, res)=> {
     const educationnal = {
         name: "Educational Methods and Experimental Training",
-        url : "#",
+        url : "/mindmap/Educational_robotic_computational_thinking",
         children: [
             { name: "Technological applications" , url:"/mindmap/Educational_robotic_computational_thinking/technological_apps" },
             { name: "Robotics and Education", url:"/mindmap/Educational_robotic_computational_thinking/robotics_education"  }
@@ -77,9 +77,10 @@ app.get('/mindmap/Educational_robotic_computational_thinking',(req, res)=> {
 app.get('/mindmap/creativity_problem_Solving',(req, res)=> {
     const crea_problem = {
         name: "Creativity and Problem Solving",
-        url : "#",
+        url : "/mindmap/creativity_problem_Solving",
         children: [
-            { name: "Theorical & Methodological Frameworks", url:"/mindmap/creativity_problem_Solving/theoric_methodologic" }
+            { name: "Theorical & Methodological Frameworks", url:"/mindmap/creativity_problem_Solving/theoric_methodologic" },
+            { name: "Pratical approach in problem solving", url:"/mindmap/creativity_problem_Solving/Pratical_approach" }
         ]
     };
     res.json(crea_problem);
@@ -124,6 +125,9 @@ app.get('/mindmap/Educational_robotic_computational_thinking/robotics_education'
 
 app.get('/mindmap/creativity_problem_Solving/theoric_methodologic', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/', 'theoric_methodologic.html'));
+});
+app.get('/mindmap/creativity_problem_Solving/pratical_approach', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/', 'pratical_approach.html'));
 });
 
 
